657e0466ce316bca93d74af72ca76711f8439271
unknown <kimberly.zai@berkeley.edu>
2018-03-01 21:41:58 +0800

    Actually get Quizlet user access token. Can also retrieve user info

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   });
 
   function authorize() {
-    var csrf_token = "pineapple"; // Change later to cryptographically secure token
+    var csrf_token = "pineapple"; // TODO: Change later to cryptographically secure token
     var redirect_uri = chrome.identity.getRedirectURL("quizlet_cb");
     var url_params = {
       response_type: "code",
@@ -20,9 +20,58 @@ $(document).ready(function() {
         const url = new URL(redirect_url);
         const params = new URLSearchParams(url.search);
 
-        var received_state = params.get("state");
-        var access_token = params.get("code");
+        alert(params);
+
+        var state = params.get("state");
+        var code = params.get("code");
+
+        // TODO: if state is correct
+        $.ajax
+        ({
+          type: "POST",
+          url: "https://api.quizlet.com/oauth/token",
+          data: {
+            grant_type: "authorization_code",
+            code: code,
+            redirect_uri: redirect_uri,
+          },
+          headers: {
+            "Authorization": "xxx",
+          },
+          dataType: "json",
+          success: function (data) {
+            alert("access token: " + data.access_token + "\nuser_id: " + data.user_id);
+            console.info(data);
+            getUserData(data);
+          },
+          error: function(jqXHR, textStatus, error) {
+            alert("There was a problem. Please try logging in again.");
+          }
+        });
       }
     );
+  }
+
+  function getUserData(data) {
+    $.ajax
+    ({
+      url: "https://api.quizlet.com/2.0/users/" + data.user_id,
+      headers: {
+        "Authorization": "Bearer " + data.access_token,
+      },
+      dataType: "json",
+      success: function (data) {
+        alert("SUCCESS");
+        var numSets = data.sets.length;
+        console.log(numSets);
+        console.log(data);
+        $(data.sets).each(function() {
+          console.log($(this));
+        });
+      },
+      error: function(jqXHR, textStatus, error) {
+        alert("Error. Could not retrieve your flashcard sets.");
+      }
+    });
   }
 });
