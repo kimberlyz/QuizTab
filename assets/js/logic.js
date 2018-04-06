fa08947287e7036eec9fa195d8a437a62ce17943
unknown <kimberly.zai@berkeley.edu>
2018-04-06 22:46:13 +0800

    Restructure code and implement flashcard flipping

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,19 @@
+/**
+ * PG01 - Login and Authentication
+ * PG02 - Flashcard Sets Display
+**/
 $(document).ready(function() {
 
+  $("#sets-page").addClass("hide");
   load_background();
-  $("a").click(function() {
+
+  $("#login-button").click(function() {
     authorize();
   });
 
+/* PG01 - Login and Authentication
+===============================================================================
+*/
   function load_background() {
     var img_tag = new Image();
 
@@ -78,8 +87,79 @@ $(document).ready(function() {
       })
     });
 
+    // TODO: Refactor this
     await Promise.all([saveAccessTokenPromise, saveUserIDPromise]).then(function(values) {
-      window.location.replace("../../sets.html");
+      $("#login-page").addClass("hide")
+      $("#sets-page").removeClass("hide");
+      $("body").addClass("blue-background");
+      $("#card").flip({
+        trigger: 'manual'
+      });
+      $("#card").click(function() {
+        $("#card").flip('toggle');
+      });
+      $(document).keypress(function(e) {
+        if (e.which == 32) {
+          $("#card").flip('toggle');
+        }
+      });
+      getUserDataAndSets();
     });
+  }
+
+/* PG02 - Flashcard Sets Display
+===============================================================================
+*/
+  async function getUserDataAndSets() {
+    var getAccessTokenPromise = new Promise(function (resolve, reject) {
+      chrome.storage.sync.get(['access_token'], function(result) {
+        resolve(result.access_token);
+      });
+    });
+
+    var getUserIDPromise = new Promise(function (resolve, reject) {
+      chrome.storage.sync.get(['user_id'], function(result) {
+        resolve(result.user_id);
+      });
+    });
+
+    var result = await Promise.all([
+     getAccessTokenPromise,
+     getUserIDPromise,
+    ]);
+
+    var access_token = result[0];
+    var user_id = result[1];
+
+    getSets(access_token, user_id);
+  }
+
+  function getSets(access_token, user_id) {
+    $.ajax
+    ({
+      url: "https://api.quizlet.com/2.0/users/" + user_id,
+      headers: {
+        "Authorization": "Bearer " + access_token,
+      },
+      dataType: "json",
+      success: function (data) {
+        populateSetTitles(data.sets);
+      },
+      error: function(jqXHR, textStatus, error) {
+        alert("Error. Could not retrieve your flashcard sets.");
+      }
+    });
+  }
+
+  function populateSetTitles(dataSets) {
+    $(dataSets).each(function() {
+      console.log(this.title);
+      var setRow = "<div class='row'>"
+                    + "<h2>" + this.title + "</h2>"
+                    + "<p>Some text...</p>";
+                    + "</div>";
+      $("#container").append(setRow);
+    });
+    $("container").removeClass("hide");
   }
 });
